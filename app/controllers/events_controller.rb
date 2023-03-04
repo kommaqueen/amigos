7468bdaff8d2f89d878d9d606b31a24184e74047
Lucy Treganna lucy.treganna@gmail.com <lucy.treganna@gmail.com>
2023-03-04 14:39:22 +0100

    added events to place show page and created event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,16 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  has_many :events, dependent: :destroy
 end
