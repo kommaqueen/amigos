c1eba1e036129a1f05d8ccfd2081dc879b76acc1
Deanna Wright <deanna.j.wright@gmail.com>
2023-03-16 20:41:41 +0100

    added dashboard logic for button and opening chat, styled avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     @users = User.all
     @friendship = Friendship.new
     @chatroom = Friendship.where(status: "accepted").where(asker: @user).or(Friendship.where(status: "accepted").where(receiver: @user)).where(asker: current_user).or(Friendship.where(receiver: current_user)).take
-   
   end
 
   def index
