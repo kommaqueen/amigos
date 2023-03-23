d2660cc0d3c726dc23128c40644e70be664bc7e6
Caio Zimbardi <cbzimbardi@gmail.com>
2023-03-23 16:58:54 +0100

    fixing invites table db-drop everyone

--- db/migrate/20230307180758_create_invites.rb
+++ db/migrate/20230307180758_create_invites.rb
@@ -1,8 +1,6 @@
 class CreateInvites < ActiveRecord::Migration[7.0]
   def change
     create_table :invites do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :place, null: false, foreign_key: true
       t.boolean :accepted
 
       t.timestamps
